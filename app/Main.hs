c717e28bf036789f54ae34075de296f12ac8bc74
Glen Sargent <glensarg@hotmail.co.uk>
2024-03-10 13:55:55 +0000

    wip: make window

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,20 @@
 
 module Main where
 
+import qualified SDL
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  SDL.initializeAll
+  window <- SDL.createWindow "App" SDL.defaultWindow
+  renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+  coreLoop renderer
+  -- end
+  SDL.destroyWindow window
+
+coreLoop :: SDL.Renderer -> IO ()
+coreLoop renderer = do
+  events <- SDL.pollEvents
+  print events
+  coreLoop renderer
+
